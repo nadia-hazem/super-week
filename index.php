300be554db0a842093dfe2b3d52503b73be6c263
nedh <nedh.design@gmail.com>
2023-05-02 11:55:34 +0200

    job01 done

--- index.php
+++ index.php
@@ -4,4 +4,19 @@ require 'vendor/autoload.php';
 $router = new AltoRouter();
 $router->setBasePath('/super-week');
 
-$router->map( 'GET', '/', 'render_home', 'home' );
+/* $router->map( 'GET', '/', function() {
+    echo "<h1>Bienvenue sur l'accueil</h1>";
+}, 'home' ); */
+
+$router->addRoutes(array(
+    array('GET', '/', function() { echo "<h1>Bienvenue sur l'accueil</h1>";}, 'home'),
+    array('GET', '/users', function() { echo "<h1>Bienvenue sur la liste des Utilisateurs.</h1>";}, 'users'),
+    array('GET', '/users/[i:id]', function($id) { echo "<h1>Bienvenue sur la page de l'utilisateur $id</h1>";}, 'user'),
+));
+
+$match = $router->match();
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}

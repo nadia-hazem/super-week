e35c316646756eb37ef511234bf240f4f900a326
nedh <nedh.design@gmail.com>
2023-05-02 14:44:00 +0200

    bdd complete

--- index.php
+++ index.php
@@ -1,17 +1,50 @@
 <?php
+// variables de connexion à la bdd
+$host = 'localhost';
+$dbname = 'super-week';
+$dbUser = 'root';
+$dbPass = '';
+
+try {
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbUser, $dbPass);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->exec("set names utf8");
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+    die();
+}
+
 require 'vendor/autoload.php';
 
 // generate data by calling methods
-/* $faker = Faker\Factory::create();
+/* $faker = Faker\Factory::create(); */
 
-for ($i = 0; $i < 3; $i++) {
+/* for ($i = 0; $i < 3; $i++) {
     echo $faker->name() . "<br>";
     echo $faker->email() . "<br>";
 }
 for ($i = 0; $i < 3; $i++) {
     echo $faker->title() . "<br>";
     echo $faker->text() . "<br>";
+} */ 
+
+/* for ($i = 0; $i < 10; $i++) {
+    $firstname = $faker->firstName();
+    $lastname = $faker->lastName();
+    echo $firstname . ' ' . $lastname . '<br>';
+    // to lowercase
+    $mail = strtolower($firstname . '.' . $lastname . '@' . $faker->freeEmailDomain());
+    echo $mail . '<br>';
+    // insert
+    $req = $bdd->prepare('INSERT INTO user (first_name, last_name, email) VALUES (:firstname, :lastname, :email)');
+    $req->execute(array(
+        'firstname' => $firstname,
+        'lastname' => $lastname,
+        'email' => $mail
+    ));
 } */
+
+
 
 $router = new AltoRouter();
 $router->setBasePath('/super-week');
